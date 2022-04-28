ea08d93460f5effcc61406f888b4b09d404e876c
Rang-Rang <99758226+Rang-Rang@users.noreply.github.com>
2022-04-28 20:45:11 +0800

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,5 @@
+// navigation menu
+let nav = document.querySelector(".nav");
+nav.addEventListener("click", () => {
+    let transparent = document.querySelector(".transparent").classList.toggle("untransparent");
+});
